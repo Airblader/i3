a2d17d00ff47fc74239662509df996af5f459351
Sam Bazley <sambazley@protonmail.com>
2018-01-18 18:42:14 +0000

    Removed incorrect deco_rect else block

--- src/render.c
+++ src/render.c
@@ -72,8 +72,6 @@ void render_con(Con *con, bool render_fullscreen, bool already_inset) {
         if (con->deco_rect.width != 0 && con->deco_rect.height != 0) {
             if (con_is_leaf(con)) {
                 con->deco_rect = rect_add(con->deco_rect, inset);
-            } else {
-                con->deco_rect = inset;
             }
         }
 
