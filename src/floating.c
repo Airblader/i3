cfcd8423b26b143c04ad3c9e9edea3b376630ce9
Ingo Bürk <ingo.buerk@tngtech.com>
2018-10-23 20:02:29 +0200

    Fix compilation error.
    closes #246

--- src/floating.c
+++ src/floating.c
@@ -907,7 +907,7 @@ bool floating_reposition(Con *con, Rect newrect) {
 
     /* Workspace change will already result in a tree_render. */
     if (!reassigned) {
-        render_con(con, false);
+        render_con(con, false, true);
         x_push_node(con);
     }
     return true;
