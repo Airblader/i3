b9e7a0bdf9d9baed15581e2950ef3b48ef07e96c
Benjamin Friesen <benjamin.friesen@student.ufv.ca>
2019-04-19 11:53:41 -0700

    Fullscreen no longer has rounded corners

--- src/x.c
+++ src/x.c
@@ -470,46 +470,55 @@ static size_t x_get_border_rectangles(Con *con, xcb_rectangle_t rectangles[4]) {
 void x_shape_window(Con *con) {
 
   const xcb_query_extension_reply_t *shape_query;
-  
   shape_query = xcb_get_extension_data(conn, &xcb_shape_id);
   if (!shape_query->present) return; 
   
-  uint16_t w  = con->rect.width;
-  uint16_t h  = con->rect.height;
-  
-  xcb_pixmap_t pid = xcb_generate_id(conn);
-  
-  xcb_create_pixmap(conn, 1, pid, con->frame.id, w, h);
-  
-  xcb_gcontext_t black = xcb_generate_id(conn);
-  xcb_gcontext_t white = xcb_generate_id(conn);
-  
-  xcb_create_gc(conn, black, pid, XCB_GC_FOREGROUND, (uint32_t[]){0, 0});
-  xcb_create_gc(conn, white, pid, XCB_GC_FOREGROUND, (uint32_t[]){1, 0});
-  
-  int32_t r = 10;
-  int32_t d = r * 2;
-  
-  xcb_arc_t arcs[] = {
-                      { -1, -1, d, d, 0, 360 << 6 },
-                      { -1, h-d, d, d, 0, 360 << 6 },
-                      { w-d, -1, d, d, 0, 360 << 6 },
-                      { w-d, h-d, d, d, 0, 360 << 6 },
-  };
-  xcb_rectangle_t rects[] = {
-                             { r, 0, w-d, h },
-                             { 0, r, w, h-d },
-  };
+  if (con->fullscreen_mode) {
 
-  xcb_rectangle_t bounding = {0, 0, w, h};
-  xcb_poly_fill_rectangle(conn, pid, black, 1, &bounding);
-  xcb_poly_fill_rectangle(conn, pid, white, 2, rects);
-  xcb_poly_fill_arc(conn, pid, white, 4, arcs);
-  
-  xcb_shape_mask(conn, XCB_SHAPE_SO_SET, XCB_SHAPE_SK_BOUNDING, con->frame.id, 0, 0, pid);
-  xcb_shape_mask(conn, XCB_SHAPE_SO_SET, XCB_SHAPE_SK_CLIP, con->frame.id, 0, 0, pid);
-  
-  xcb_free_pixmap(conn, pid);
+    xcb_shape_mask(conn, XCB_SHAPE_SO_SET, XCB_SHAPE_SK_BOUNDING, con->frame.id, 0, 0, XCB_NONE);
+    xcb_shape_mask(conn, XCB_SHAPE_SO_SET, XCB_SHAPE_SK_CLIP, con->frame.id, 0, 0, XCB_NONE);
+
+  }else {
+
+    uint16_t w  = con->rect.width;
+    uint16_t h  = con->rect.height;
+    
+    xcb_pixmap_t pid = xcb_generate_id(conn);
+    
+    xcb_create_pixmap(conn, 1, pid, con->frame.id, w, h);
+    
+    xcb_gcontext_t black = xcb_generate_id(conn);
+    xcb_gcontext_t white = xcb_generate_id(conn);
+    
+    xcb_create_gc(conn, black, pid, XCB_GC_FOREGROUND, (uint32_t[]){0, 0});
+    xcb_create_gc(conn, white, pid, XCB_GC_FOREGROUND, (uint32_t[]){1, 0});
+    
+    int32_t r = 13;
+    int32_t d = r * 2;
+    
+    xcb_arc_t arcs[] = {
+                        { -1, -1, d, d, 0, 360 << 6 },
+                        { -1, h-d, d, d, 0, 360 << 6 },
+                        { w-d, -1, d, d, 0, 360 << 6 },
+                        { w-d, h-d, d, d, 0, 360 << 6 },
+    };
+    xcb_rectangle_t rects[] = {
+                               { r, 0, w-d, h },
+                               { 0, r, w, h-d },
+    };
+    
+    xcb_rectangle_t bounding = {0, 0, w, h};
+    xcb_poly_fill_rectangle(conn, pid, black, 1, &bounding);
+    xcb_poly_fill_rectangle(conn, pid, white, 2, rects);
+    xcb_poly_fill_arc(conn, pid, white, 4, arcs);
+    
+    xcb_shape_mask(conn, XCB_SHAPE_SO_SET, XCB_SHAPE_SK_BOUNDING, con->frame.id, 0, 0, pid);
+    xcb_shape_mask(conn, XCB_SHAPE_SO_SET, XCB_SHAPE_SK_CLIP, con->frame.id, 0, 0, pid);
+    
+    xcb_free_pixmap(conn, pid);
+
+  }
+
 }
 
 /*
