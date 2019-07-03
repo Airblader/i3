098c69ec075833597b42891ddbf668f64429d072
Olivier Pinon <oliv.pinon@gmail.com>
2019-07-03 20:59:03 +0200

    make the border's radius customizable with border_radius param in config

--- src/config_directives.c
+++ src/config_directives.c
@@ -318,6 +318,10 @@ CFGFUN(smart_gaps, const char *enable) {
         config.smart_gaps = eval_boolstr(enable) ? SMART_GAPS_ON : SMART_GAPS_OFF;
 }
 
+CFGFUN(border_radius, const long radius) {
+    config.border_radius = radius;
+}
+
 CFGFUN(floating_minimum_size, const long width, const long height) {
     config.floating_minimum_width = width;
     config.floating_minimum_height = height;
