00df249059bccde7d9c0d6844e25879db783504d
Olivier Pinon <oliv.pinon@gmail.com>
2019-07-04 03:00:52 +0200

    remove border by default

--- src/x.c
+++ src/x.c
@@ -480,9 +480,6 @@ void x_shape_title(Con *con){
     xcb_create_gc(conn, white, pid, XCB_GC_FOREGROUND, (uint32_t[]){1, 0});
 
     int32_t r = con->border_radius;
-    if (r == 0) {
-        r = 4;
-    }
     int32_t d = r * 2;
 
     xcb_rectangle_t bounding = {0, 0, w, h};
@@ -541,9 +538,6 @@ void x_shape_window(Con *con) {
     xcb_create_gc(conn, white, pid, XCB_GC_FOREGROUND, (uint32_t[]){1, 0});
 
     int32_t r = con->border_radius;
-    if (r == 0) {
-        r = 4;
-    }
     int32_t d = r * 2;
 
     xcb_rectangle_t bounding = {0, 0, w, h};
